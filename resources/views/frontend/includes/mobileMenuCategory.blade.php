14b9e842a8c8833305506b69ea3228a45a35737c
Md. Ripon Mia <33976306+riponahmed2201@users.noreply.github.com>
2025-03-02 11:45:30 +0600

    mobile menu doing

---
+++ resources/views/frontend/includes/mobileMenuCategory.blade.php
@@ -0,0 +1,23 @@
+<div class="offcanvas offcanvas-start canvas-mb toolbar-shop-mobile" id="toolbarShopmb">
+    <span class="icon-close icon-close-popup" data-bs-dismiss="offcanvas" aria-label="Close"></span>
+    <div class="mb-canvas-content">
+        <div class="mb-body">
+            <ul class="nav-ul-mb" id="wrapper-menu-navigation">
+
+                <li class="nav-mb-item">
+                    <a href="shop-default.html" class="tf-category-link mb-menu-link">
+                        <div class="image">
+                            <img src="assets/frontend/images/shop/cate/cate1.jpg" alt="">
+                        </div>
+                        <span>Accessories</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <div class="mb-bottom">
+            <a href="shop-default.html" class="tf-btn fw-5 btn-line">View all collection<i
+                    class="icon icon-arrow1-top-left"></i></a>
+        </div>
+    </div>
+</div>
